56c4aacec5aa93ef8f8429ec9051856409b4ff8d
Sam Tapia <stapi010@ucr.edu>
2021-03-04 22:34:43 -0800

    added twitter logo

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import tweetLogo from './tweet.png'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src={tweetLogo} className="App-logo" alt="logo"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          TweetyFind
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/neztEx/tweetyfind"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Git-Repo
         </a>
       </header>
     </div>
--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
-ReactDOM.render(<h1>Hello World!</h1>,document.getElementById('test'))
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
